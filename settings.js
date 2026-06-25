152fb46d185a13aec4dc83595918e66ddcc053cd
zero one <codezerocomunity@gmail.com>
2026-06-26 01:34:48 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Kenny Bot',
-  author: 'Wambugu Kinyua',
-  botName: "Kinyua Bot",
-  botOwner: 'Wambugu Kinyua', // Your name
-  ownerNumber: '2547xxxxxx', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'ZERO Bot',
+  author: 'HUNTER ZEN',
+  botName: "ZERO Bot",
+  botOwner: 'HUNTER ZEN', // Your name
+  ownerNumber: '254712740695', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
